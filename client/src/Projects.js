c0b6091cf8acf2ba23a1a77ecd814de1d8cc3263
EliasHaynes <eliasmhaynes@gmail.com>
2023-12-11 23:33:08 -0600

    Responsive changes to Projects

--- client/src/Projects.js
+++ client/src/Projects.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import vehicleImg from "./assets/Capstone-Icon-Ss.png"
-import jobImg from "./assets/Screenshot 2023-12-02 200024.png"
-import tutorImg from "./assets/Screenshot 2023-12-02 195841.png"
+import vehicleImg from "./assets/Capstone-Icon-Ss.png";
+import jobImg from "./assets/Screenshot 2023-12-02 200024.png";
+import tutorImg from "./assets/Screenshot 2023-12-02 195841.png";
 
 function Projects() {
-  const [currentProject, setCurrentProject] = useState("Job"); // "Vehicle"|| "Job" || "Tutor"
+  const [currentProject, setCurrentProject] = useState("Vehicle"); // "Vehicle"|| "Job" || "Tutor"
 
   function displayCurrentProject() {
     switch (currentProject) {
@@ -20,7 +20,9 @@ function Projects() {
     function Vehicle() {
       return (
         <ProjectContainer>
-          <ProjectImg src={vehicleImg}/>
+          <ProjectImgContainer>
+            <ProjectImg src={vehicleImg} className="vehicle-img" />
+          </ProjectImgContainer>
           <h4>Vehicle</h4>
           <ProjectSkills>
             <span>Vehicle</span>
@@ -31,7 +33,7 @@ function Projects() {
             <span></span>
           </ProjectSkills>
           <h4>Watch this presentation of the project</h4>
-          <iframe
+          <ProjectVideo
             width="560"
             height="315"
             src="https://www.youtube.com/embed/eN4wCX8p2Mc?si=MGWMIN3ob9GUojD0"
@@ -39,7 +41,7 @@ function Projects() {
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen
-          ></iframe>
+          ></ProjectVideo>
           <h4>
             Link to project repository:{" "}
             <a href="https://github.com/EliasHaynes/capstone">
@@ -49,10 +51,13 @@ function Projects() {
         </ProjectContainer>
       );
     }
+
     function Job() {
       return (
         <ProjectContainer>
-          <ProjectImg src={jobImg}/>
+          <ProjectImgContainer>
+            <ProjectImg src={jobImg} />
+          </ProjectImgContainer>
           <h4>Job</h4>
           <ProjectSkills>
             <span>job</span>
@@ -63,7 +68,7 @@ function Projects() {
             <span></span>
           </ProjectSkills>
           <h4>Watch this presentation of the project</h4>
-          <iframe
+          <ProjectVideo
             width="560"
             height="315"
             src="https://www.youtube.com/embed/eN4wCX8p2Mc?si=MGWMIN3ob9GUojD0"
@@ -71,7 +76,7 @@ function Projects() {
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen
-          ></iframe>
+          ></ProjectVideo>
           <h4>
             Link to project repository:{" "}
             <a href="https://github.com/theMVPshop/jobAppTracker">
@@ -81,10 +86,14 @@ function Projects() {
         </ProjectContainer>
       );
     }
+
     function Tutor() {
       return (
         <ProjectContainer>
-          <ProjectImg src={tutorImg}/>
+          <ProjectImgContainer>
+            <ProjectImg src={tutorImg} />
+          </ProjectImgContainer>
+
           <h4>Tutor</h4>
           <ProjectSkills>
             <span>Tutor</span>
@@ -95,15 +104,13 @@ function Projects() {
             <span></span>
           </ProjectSkills>
           <h4>Watch this presentation of the project</h4>
-          <iframe
-            width="560"
-            height="315"
+          <ProjectVideo
             src="https://www.youtube.com/embed/eN4wCX8p2Mc?si=MGWMIN3ob9GUojD0"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen
-          ></iframe>
+          ></ProjectVideo>
           <h4>
             Link to project repository:{" "}
             <a href="https://github.com/theMVPshop/chatbottutor">
@@ -152,13 +159,13 @@ const Container = styled.div`
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
-  width: 100%;
+  width: 100vw;
 `;
 
 const ProjectContainer = styled.div`
-    display:flex;
-    flex-flow:column nowrap;
-    align-items:center;
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
 `;
 
 const PortfolioNav = styled.div`
@@ -167,8 +174,7 @@ const PortfolioNav = styled.div`
 `;
 
 const ProjectSkills = styled.div`
-
-    margin-right: 2%;
+  margin-right: 2%;
 `;
 
 const ButtonNav = styled.button`
@@ -180,9 +186,25 @@ const SelectedButtonNav = styled(ButtonNav)`
   border-bottom: 3px solid red;
 `;
 
+const ProjectImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  margin: 1px;
+  width: 50%;
+  height: 50%;
+`;
+
 const ProjectImg = styled.img`
-    width:50%;
-    height:50%;
+  border: 1px solid blue;
+  width: 75vw;
+  height: 30vh;
+  max-width: 900px;
+  max-height: 900px;
+`;
+
+const ProjectVideo = styled.iframe`
+  width: 75%;
+  height: 75%;
 `;
 
 export default Projects;
